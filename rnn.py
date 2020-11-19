8773eb436b0861c81dd35d578bb0fea87a321b06
abedmir31 <abedmir31@gmail.com>
2020-11-19 17:20:10 -0600

    Forward pass

--- rnn.py
+++ rnn.py
@@ -4,54 +4,84 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
+
 # Raw url for stock data 
 url = "https://raw.githubusercontent.com/AbedMir31/StockMarketRNN/main/SPY.csv"
 
+headers = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+data = pd.read_csv(url, sep = ',', names = headers,skiprows = 1)
+data = data.drop('Adj Close', axis=1)
+data = data.drop('Date', axis=1)
+
 training_size = .8
-train_data = pd.read_csv(url)
-# training_data, test_data = train_test_split(data, train_size = training_size, shuffle = False)
-#print(train_data.head)
-train_data = train_data.iloc[:,4].values
+train_dataset, test_dataset = train_test_split(data,train_size = training_size, shuffle = False)
 scaler = MinMaxScaler()
-train_data = scaler.fit_transform(train_data.reshape(-1,1))
+train_dataset = scaler.fit_transform(train_dataset.values)
+# print(train_dataset)
 
-timestep = 40
-x_train = []
-y_train = []
-for i in range (timestep, len(train_data)):
-    x_train.append(train_data[i-timestep:i, 0])
-    y_train.append(train_data[i,0])
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
+X = []
+Y = []
+timestep = 20
+for i in range(timestep,len(train_dataset)):
+  X.append(train_dataset[i - timestep:i,3])#[0,1,2,4]])
+  Y.append(train_dataset[i,3])
+X = np.array(X)
+Y = np.array(Y)
+'''
+print('X', X.shape)
+print('Y', Y.shape)
+print('X',X[0])
+'''
 
-price_dim = len(train_data)
-hidden_dim = 100
-bptt_truncate = 4
+input_layer = 1
+hidden_layer = 100
 
-U = np.random.uniform(-np.sqrt(1./price_dim), np.sqrt(1./price_dim), (hidden_dim, price_dim))
-V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (price_dim, hidden_dim))
-W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+# Weights
+U = np.random.randn(input_layer, hidden_layer)
+V = np.random.randn(hidden_layer, input_layer)
+W = np.random.randn(hidden_layer, hidden_layer)
+
+# Bias layers, bias_hidden and bias_output
+bh = np.random.randn(input_layer, hidden_layer)
+bo = np.random.randn(input_layer, input_layer)
+
 
 # We want to use the softmax activation function in the RNN
 def softmax(x):
     e = np.exp(x)
     return e / e.sum()
+def tanh(x):
+    return (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
 
-def forward_propagate(x):
-    T = timestep
-    s = np.zeros((T+1, hidden_dim))
-    s[-1] = np.zeros(hidden_dim)
-    o = np.zeros((T, price_dim))
-    for i in np.arange(T):
-        s[i] = np.tanh(U[:,int(x[i])] + W.dot(s[i-1]))
-        o[i] = softmax(V.dot(s[i]))
-    return [o, s]
+def forward_propagate(inputs, hidden_state):
+    outputs, hidden_states = [], []
+    for t in range(len(inputs)):
+        # CODE - compute new hidden state use old hidden state
+        # temp  = U*x + h(t-1)*W +bh
 
-def predict_state(x):
-    o, s = forward_propagate(x)
-    return np.argmax(o, axis=1)
+        temp1 = np.dot(U,inputs[t])
+        temp2 = np.dot(hidden_state[t],W) + bh
+        hidden_state[t+1] = tanh(temp1 + temp2)
+        # CODE - compute output
+        
+        out = np.dot(hidden_state[t+1],V) + bo
 
-predict = predict_state(x_train[10])
-print(predict.shape)
-print(predict)
+        outputs.append(out)
+        hidden_states.append(hidden_state[t+1].copy())
+    return outputs, hidden_state
+'''
+def back_propagate(x):
+    
+def update_weights(x):
+'''
+def train(x):
+    hidden_state = []
+    for example in range(0,2):
+        for i in range(0,timestep+1):
+            hidden_state.append(np.zeros((input_layer,hidden_layer)))
+        o, h = forward_propagate(x[example], hidden_state)
+        '''
+        back_propagate()
+        update_weights()
+        '''
+train(X)
